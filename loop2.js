cb7e2596ebb0c4fb294c309b1e1ec0cddbdf2d3f
pbahadian <46781071+pbahadian@users.noreply.github.com>
2019-04-09 13:31:39 +0200

    Add files via upload

---
+++ loop2.js
@@ -0,0 +1,26 @@
+"use strict"
+
+/*
+var array_name = ['Paula Bahadian Moreira'];
+array_name.forEach(function(elemento) {
+	console.log("Nombre: "+ elemento);
+}
+);
+
+
+console.log(Array.from('Paula Bahadian Moreira'));
+
+
+var str="this is my new tutorial";
+
+var a1 = new Array();
+
+a1=str.split(" ");
+
+console.log(a1.join(" <br> "));
+*/
+var array_name =['Paula Bahadian Moreira'];
+array_name.forEach(letter(array_name){
+console.log(str.split(',').join("\r\n"));
+}
+);
